ab8042ffa6baf4fc5cdcfa0d1ea7d71a80d2e12b
Msjdleech <106671274+Msjdleech@users.noreply.github.com>
2022-06-26 09:36:27 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -8,9 +8,9 @@ except ImportError:
         API_HASH = [str, "abcdedf......"]
         API_ID = [int, 1234567]
         BOT_TOKEN = [str, "bot:token here"]
-        COMPLETED_STR = [str, "★ 🅸 🅻🅾🆅🅴 🆈🅾🆄 ★"]
-        REMAINING_STR = [str, "★ 🄸 🄻🄾🅅🄴 🅈🄾🅄 ★"]
-        MAX_QUEUE_SIZE = [int, 1]
+        COMPLETED_STR = [str, "💝"]
+        REMAINING_STR = [str, "🤍"]
+        MAX_QUEUE_SIZE = [int, 5]
         SLEEP_SECS = [int, 10]
         IS_MONGO = [bool, False]
 
